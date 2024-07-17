33aa02fcfa0f38c8d12cc9bfbd93b4906a9a259f
Ramesh Tekumudi <ramesh2014tk@gmail.com>
2024-07-17 15:14:35 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by RAMESH on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
